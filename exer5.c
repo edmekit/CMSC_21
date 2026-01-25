0bd4bfd75a3a35637e682a6efa632750cb20d85c
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2026-01-25 11:27:35 +0800

    pointers cuh

--- exer5.c
+++ exer5.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int words[10];
+int manageWords(char words[][5], int *size);
+int printWordPool(char words[][5], int *size);
+int insertWord(char words[][5], int *size);
 
 int main(){
+    char words[][5] = {"feel", "teal", "reel"};
+    int number = sizeof(words) / sizeof(words[0]);
+    int* size = &number;
     int menu;
     printf("Welcome to Terminal Wordle.\n");
     printf("[1] Play the game.\n");
@@ -13,13 +18,14 @@ int main(){
     printf("Choose an option: "); scanf("%i", &menu);
 
     if (menu == 2){
-        manageWords();
+        manageWords(words, size);
     } else {
         return 0;
     }
 }
 
-int manageWords(){
+int manageWords(char words[][5], int *size){
+    while (1){
     int menu;
     printf("Manage Words\n");
     printf("[1] Print Word Pool\n");
@@ -31,24 +37,31 @@ int manageWords(){
     printf("Choose an option: "); scanf("%i", &menu);
 
     if (menu == 1){
-        printWordPool();
+        printWordPool(words, size);
     } else if (menu == 2){
-        insertWord();
+        insertWord(words, size);
     } else if (menu == 3){
-        deleteWord();
-    } else if (menu == 4){
-        resetWord();
+        deleteWord(words, size);
     }
 }
+}
 
-int printWordPool(){
-    for (int i = 0; i < 10; i++){
+int printWordPool(char words[][5], int *size){
+    printf("Current size: %d\n", *size);
+    for (int i = 0; i < *size; i++){
         printf("%s\n", words[i]);
     }
 }
 
-int insertWord(){
+int insertWord(char words[][5], int *size){
     char word[5];
     printf("Enter Word to insert: "); scanf("%s", word);
+    for (int i = 0; i < 5; i++){
+        words[*size][i] = word[i];
+    }
+    (*size)++;
+}
+
+int deleteWord(char words[][5], int *size){
     
 }
