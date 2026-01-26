15ca1961a0d05c26186b8b09c23f7722261c9d76
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2026-01-26 08:52:13 +0800

    these mallocs bruh

--- exer5.c
+++ exer5.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int manageWords(char words[][5], int *size);
-int printWordPool(char words[][5], int *size);
-int insertWord(char words[][5], int *size);
-int deleteWord(char words[][5], int *size);
-int resetWords(char words[][5], int *size);
+int manageWords(char (*words)[5], int *size, int *capacity);
+int printWordPool(char (*words)[5], int *size);
+int insertWord(char (**words)[5], int *size, int *capacity);
+int deleteWord(char (*words)[5], int *size);
+int resetWords(int *size);
 int playGame();
 
 int main(){
-    char words[][5] = {"feel", "teal", "reel"};
-    int number = sizeof(words) / sizeof(words[0]);
-    int* size = &number;
     int menu;
+    char (*words)[5] = NULL;
+    int size = 0;
+    int capacity = 3;
+
+    words = malloc(capacity * sizeof(*words));
+
     printf("Welcome to Terminal Wordle.\n");
     printf("[1] Play the game.\n");
     printf("[2] Manage words.\n");
@@ -23,8 +27,9 @@ int main(){
         playGame();
     }
     if (menu == 2){
-        manageWords(words, size);
+        manageWords(words, &size, &capacity);
     } else {
+        free(words);
         return 0;
     }
 }
@@ -33,7 +38,7 @@ int playGame(){
     //making onli
 }
 
-int manageWords(char words[][5], int *size){
+int manageWords(char (*words)[5], int *size, int *capacity){
     while (1){
     int menu;
     printf("Manage Words\n");
@@ -48,11 +53,11 @@ int manageWords(char words[][5], int *size){
     if (menu == 1){
         printWordPool(words, size);
     } else if (menu == 2){
-        insertWord(words, size);
+        insertWord(&words, size, capacity);
     } else if (menu == 3){
         deleteWord(words, size);
     } else if (menu == 4){
-        resetWords(words, size);
+        resetWords(size);
     } else if (menu == 0){
         return 0;
     } else {
@@ -61,26 +66,31 @@ int manageWords(char words[][5], int *size){
 }
 }
 
-int printWordPool(char words[][5], int *size){
+int printWordPool(char (*words)[5], int *size){
     printf("Current size: %d\n", *size);
     for (int i = 0; i < *size; i++){
         printf("%s\n", words[i]);
     }
 }
 
-int insertWord(char words[][5], int *size){
+int insertWord(char (**words)[5], int *size, int *capacity){
+    if (*size == *capacity){
+        *capacity *= 2;
+        *words = realloc(*words, *capacity * sizeof(*(*words)));
+    }
+
     char word[5];
-    printf("Enter Word to insert: "); scanf("%s", word);
+    printf("Enter Word to insert: "); scanf("%4s", word);
     for (int i = 0; i < 5; i++){
-        words[*size][i] = word[i];
+        *words[*size][i] = word[i];
     }
     (*size)++;
 }
 
-int deleteWord(char words[][5], int *size){
+int deleteWord(char (*words)[5], int *size){
     char word[5];
     printWordPool(words, size);
-    printf("Enter word to delete: ") ; scanf("%s", word);
+    printf("Enter word to delete: ") ; scanf("%4s", word);
 
     int position = -1; 
 
@@ -105,6 +115,6 @@ int deleteWord(char words[][5], int *size){
     (*size)--;
 }
 
-int resetWords(char words[][5], int *size){
+int resetWords(int *size){
     *size = 0;
 }
